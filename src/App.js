69b064c6041f01075687e52661079b1806518ec9
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-01-08 22:35:13 +0900

    Fix: App.js : 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Signin from "./Signin";
+import Signup from "./Signup";
 import Home from "./Home";
 import style from "./App.module.css";
 
 function App() {
-  return (
-    <div className={style.app}>
-      <Home />
-    </div>
-  );
+    return (
+        <div className={style.app}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Signin />} />
+                    <Route path="/sign_up" element={<Signup />} />
+                    <Route path="/home" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
