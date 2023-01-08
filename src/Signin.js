a46cb5f345376c5a89351f06691a942af76b8ddc
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-01-08 22:36:24 +0900

    Fix: Signin.js, Signup.js, Signin.module.css, package.json : 로그인&회원가입 API연결

--- src/Signin.js
+++ src/Signin.js
@@ -1,14 +1,51 @@
 import style from './Signin.module.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
+
 
 function Signin() {
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+
+    const changeId = (event)=>{
+        setId(event.target.value)
+    }
+    const changePw = (event)=>{
+        setPw(event.target.value)
+    }
+    const server = 'http://localhost:8080';
+    const params = {
+        "email": id,
+        "password": pw
+    }
+    const login = () => {
+        console.log('login', params);
+        axios.post(server + '/users/login', params)
+        .then(res=>{
+            console.log(res);
+            if(res.status === 200){
+                window.localStorage.setItem("token", res.data.token);
+                document.location.pathname = '/home';
+                console.log(document.location)
+            }
+        })
+    };
+    const onKeyEnter = (event)=>{
+        if(event.key === 'Enter'){
+            login();
+        }
+    }
     return (
         <div className={style.sign_in}>
             <h1><div>같이해</div><div>가치해</div></h1>
             <div className={style.content}>
-                <input type="text" placeholder='아이디'/>
-                <input type="password" placeholder='비밀번호'/>
-                <button>로그인</button>
-                <span>회원가입</span>
+                <input type="text" placeholder='아이디' value={id} onChange={changeId}/>
+                <input type="password" placeholder='비밀번호' onChange={changePw} onKeyUp={onKeyEnter}/>
+                <button onClick={login}>로그인</button>
+                <span className={style.signup}>
+                    <Link to="/sign_up">회원가입</Link>
+                </span>
             </div>
         </div>
         
--- src/Signup.js
+++ src/Signup.js
@@ -1,14 +1,67 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Swal from 'sweetalert2'
 import style from './Signin.module.css';
 
 function Signup() {
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+
+    const changeId = (event)=>{
+        setId(event.target.value)
+    }
+    const changePw = (event)=>{
+        setPw(event.target.value)
+    }
+    const server = 'http://localhost:8080';
+    const params = {
+        "email": id,
+        "password": pw
+    }
+    const signUp = () => {
+        console.log('signUp', params);
+        axios.post(server + '/users/create', params)
+        .then(res=>{
+            console.log('signUp',res);
+            if(res.status === 200){
+                window.localStorage.setItem("token", res.data.token);
+                Swal.fire({
+                    icon: 'success',
+                    title: '회원가입 성공',
+                    confirmButtonColor: '#3085d6',
+                }).then((result) => {
+                    if (result.isConfirmed || result.isDismissed) {
+                        document.location.pathname = '/';
+                    }
+                });
+            }
+        }).catch(e=>{
+            Swal.fire({
+                icon: 'error',
+                title: '회원가입 실패',
+                text: e.response.data.details,
+                confirmButtonColor: '#d33',
+            });
+        })
+    };
+    const onKeyEnter = (event)=>{
+        if(event.key === 'Enter'){
+            signUp();
+        }
+    }
     return (
         <div className={style.sign_in}>
             <h1><div>같이해</div><div>가치해</div></h1>
             <div className={style.content}>
-                <input type="text" placeholder='아이디'/>
-                <input type="password" placeholder='비밀번호'/>
-                <input type="password" placeholder='비밀번호 재확인'/>
-                <button>회원가입</button>
+                <input type="text" placeholder='아이디' onChange={changeId}/>
+                <input type="password" placeholder='비밀번호' onChange={changePw} onKeyUp={onKeyEnter}/>
+                {/* <input type="password" placeholder='비밀번호 재확인'/> */}
+                <button onClick={signUp}>회원가입</button>
+                
+                <span className={style.signup}>
+                    <Link to="/">로그인 페이지로 돌아가기</Link>
+                </span>
             </div>
         </div>
         
