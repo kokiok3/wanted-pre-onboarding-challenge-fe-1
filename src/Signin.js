b753e3ff41f96cc7c9949bf9d34cc06574e75d6b
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-01-12 23:13:31 +0900

    Fix: Signin.js: 로그인 유효성 추가

--- src/Signin.js
+++ src/Signin.js
@@ -6,6 +6,7 @@ import {Link} from 'react-router-dom';
 
 function Signin() {
     const [id, setId] = useState('');
+    const [errMsg, setErrMsg] = useState('');
     const [pw, setPw] = useState('');
 
     const changeId = (event)=>{
@@ -19,34 +20,38 @@ function Signin() {
         "email": id,
         "password": pw
     }
-    const login = () => {
-        console.log('login', params);
+    const login = (evt) => {
+        evt.preventDefault();
+        // console.log('login', params, evt);
+        
         axios.post(server + '/users/login', params)
         .then(res=>{
-            console.log(res);
             if(res.status === 200){
                 window.localStorage.setItem("token", res.data.token);
                 document.location.pathname = '/home';
                 console.log(document.location)
             }
         })
+        .catch(e=>{
+            setErrMsg((prev)=>{
+                return prev = e.response.data.details;
+            });
+        })
     };
-    const onKeyEnter = (event)=>{
-        if(event.key === 'Enter'){
-            login();
-        }
-    }
     return (
         <div className={style.sign_in}>
             <h1><div>같이해</div><div>가치해</div></h1>
-            <div className={style.content}>
-                <input type="text" placeholder='아이디' value={id} onChange={changeId}/>
-                <input type="password" placeholder='비밀번호' onChange={changePw} onKeyUp={onKeyEnter}/>
-                <button onClick={login}>로그인</button>
+            <form className={style.content} onSubmit={login}>
+                <input type="email" placeholder='아이디' value={id} onChange={changeId} required/>
+                <input type="password" placeholder='비밀번호' onChange={changePw} required/>
+
+                {errMsg ? <span className={style.errMsg}>⚠ {errMsg}</span> : null}
+
+                <button type='submit'>로그인</button>
                 <span className={style.signup}>
                     <Link to="/sign_up">회원가입</Link>
                 </span>
-            </div>
+            </form>
         </div>
         
     );
